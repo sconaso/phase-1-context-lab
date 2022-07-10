ec4a4c4ca7d4aef6ee182522dbe8ca143b26ce5e
Scott Nason <scottmnason@gmail.com>
2022-07-10 12:19:55 -0600

    commit tree 7b891a93aa3167fcc8a33ab7af7e8ebd06b51a1d

--- index.js
+++ index.js
@@ -1,4 +1,89 @@
-/* Your Code Here */
+function createEmployeeRecord(array){
+    const firstName = array[0]
+    const familyName = array[1]
+    const title = array[2]
+    const payPerHour = array[3]
+    const timeInEvents = []
+    const timeOutEvents = []
+    const employeeRecord = {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents,
+        timeOutEvents
+    }
+    return employeeRecord
+}
+
+function createEmployeeRecords(arrays){
+    const employeeRecords = arrays.map((array) => createEmployeeRecord(array))
+    return employeeRecords
+}
+
+function createTimeInEvent(timestamp){
+    const timeArray = timestamp.split(' ')
+    const date = timeArray[0]
+    const time = timeArray[1]
+    const hour = parseInt(time, 10)
+
+    this.timeInEvents.push(
+        {
+            type: 'TimeIn',
+            hour: hour,
+            date: date
+        }
+    )
+    return this
+}
+
+function createTimeOutEvent(timestamp){
+    const timeArray = timestamp.split(' ')
+    const date = timeArray[0]
+    const time = timeArray[1]
+    const hour = parseInt(time, 10)
+    
+    this.timeOutEvents.push(
+        {
+            type: 'TimeOut',
+            hour: hour,
+            date: date
+        }
+    )
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    const targetInObj = this.timeInEvents.find(obj => obj.date === date)
+    const timeIn = targetInObj.hour
+
+    const targetOutObj = this.timeOutEvents.find(obj => obj.date === date)
+    const timeOut = targetOutObj.hour
+
+    return (timeOut - timeIn) / 100
+}
+
+function wagesEarnedOnDate(date){
+    // indexTest.js invokes wagesEarnedOnDate.call(cRecord, date)
+    // using hoursWorkedOnDate.call(this, date) will use the context of cRecord
+    const hoursWorked = hoursWorkedOnDate.call(this, date)
+    return hoursWorked * this.payPerHour
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    const matchedEmployee = srcArray.find(emp => emp.firstName === firstName)
+    return matchedEmployee
+}
+
+function calculatePayroll(empRecords){
+    
+    const payPerEmp = empRecords.map((emp) => allWagesFor.call(emp))
+    const totalPayroll = payPerEmp.reduce((current, total) => current + total, 0)
+    return totalPayroll
+    
+    //const payroll = array.reduce((total, employee) => total + allWagesFor.call(employee), 0)
+    //return payroll
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
